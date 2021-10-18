9367dfbaf4bbb32272eaa1ab382b9bbc41ee9be3
Stefan Moises <beffy@proudcommerce.com>
2021-10-18 18:56:15 +0200

    Display aModuleControllers, replace oxRegistry

--- pc_oxconfig-modulsettings.php
+++ pc_oxconfig-modulsettings.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package oxconfig-modulesettings
- * @version 1.0.0
+ * @version 1.1.0
  * @url     https://github.com/proudcommerce/oxconfig-modulsettings
  *
  * @author  Stefan Moises <beffy@proudcommerce.com>
@@ -10,6 +10,8 @@
 
 require_once dirname(__FILE__) . "/bootstrap.php";
 
+use OxidEsales\Eshop\Core\Registry;
+
 // CHANGE THIS!!!
 $secretKey = 'adc92rwsfvjsdfkSS093tergAskdfs';
 $msg = "";
@@ -17,7 +19,7 @@ $msg = "";
 function resetModule($sModuleId, $search = '')
 {
     $ret = "<p>Resetting module: $sModuleId</p>";
-    $oConfig = oxRegistry::getConfig();
+    $oConfig = Registry::getConfig();
     $aModules = $oConfig->getShopConfVar('aModules');
     // there may be old module chain keys inside the array!
     if ($search !== '') {
@@ -96,13 +98,16 @@ $moduleId = trim($_POST['moduleid']);
 $toreplace = trim($_POST['toreplace']);
 $secretOK = ($secret !== '' && $secret === $secretKey);
 
-$oConfig = oxRegistry::getConfig();
+$oConfig = Registry::getConfig();
+$sModules = $sDisabledModules = $sModuleEvents = $sModuleFiles = $sModuleVersions = $sModuleTemplates = $sModuleControllers = '';
+
 $aModules = $oConfig->getShopConfVar('aModules');
 $aDisabledModules = $oConfig->getShopConfVar('aDisabledModules');
 $aModuleEvents = $oConfig->getShopConfVar('aModuleEvents');
 $aModuleFiles = $oConfig->getShopConfVar('aModuleFiles');
 $aModuleVersions = $oConfig->getShopConfVar('aModuleVersions');
 $aModuleTemplates = $oConfig->getShopConfVar('aModuleTemplates');
+$aModuleControllers = $oConfig->getShopConfVar('aModuleControllers');
 
 if ($encoded !== '' && $decoded === '') {
     $decoded = var_export(unserialize($encoded), true);
@@ -116,6 +121,7 @@ if ($encoded !== '' && $decoded === '') {
         $sModuleFiles = var_export($aModuleFiles, true);
         $sModuleVersions = var_export($aModuleVersions, true);
         $sModuleTemplates = var_export($aModuleTemplates, true);
+        $sModuleControllers = var_export($aModuleControllers, true);
     }
 }
 
@@ -153,7 +159,7 @@ if ($secretOK) {
         	<h2 class="subtitle">Enter secret to submit!</h2>
             <input class="input" type="password" size="30" name="secret" value="<?php echo $secret; ?>" size="30"/>
             <br><br>
-            <h2 class="subtitle">Reset module by ID (module will be removed from aModules, aModuleEvents, aModuleFiles, aDisabledModules, aModuleVersions, aModuleTemplates)</h2>
+            <h2 class="subtitle">Reset module by ID (module will be removed from aModules, aModuleEvents, aModuleFiles, aDisabledModules, aModuleVersions, aModuleTemplates, aModuleControllers)</h2>
             <input class="input" type="text" placeholder="exonn_sengines" name="moduleid" value="<?php echo $moduleId; ?>" size="30"/>
             <br>
             <br>
@@ -179,6 +185,9 @@ if ($secretOK) {
             <br>
             <h2 class="subtitle">Unserialized aModuleTemplates value (display only)</h2>
             <textarea class="textarea" name="decvalue" rows="6" cols="100" disabled="disabled"><?php echo $sModuleTemplates;?></textarea>
+            <br>
+            <h2 class="subtitle">Unserialized aModuleControllers value (display only)</h2>
+            <textarea class="textarea" name="decvalue" rows="6" cols="100" disabled="disabled"><?php echo $sModuleControllers;?></textarea>
             </form>
         </div>
     </body>
